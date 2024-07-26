7c2f48733448f38d68f62b7b0318f18c5888a01a
Baliqis <sulaimabaliqis@gmail.com>
2024-07-26 09:44:00 +0100

    first commit

--- src/main/java/africa/semicolon/com/quagga/QuaggaApplication.java
+++ src/main/java/africa/semicolon/com/quagga/QuaggaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QuagaApplication {
+public class QuaggaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(QuagaApplication.class, args);
+        SpringApplication.run(QuaggaApplication.class, args);
     }
 
 }
--- src/test/java/africa/semicolon/com/quagga/QuaggaApplicationTests.java
+++ src/test/java/africa/semicolon/com/quagga/QuaggaApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class QuagaApplicationTests {
+class QuaggaApplicationTests {
 
     @Test
     void contextLoads() {
