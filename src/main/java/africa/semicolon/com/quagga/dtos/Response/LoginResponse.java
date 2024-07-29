611fde11b86ea5e0762656be5197289c995335fd
josephEmeka <josephemekaonah@gmail.com>
2024-07-29 19:20:20 +0100

    custome username and password authentication

---
+++ src/main/java/africa/semicolon/com/quagga/dtos/Response/LoginResponse.java
@@ -0,0 +1,13 @@
+package africa.semicolon.com.quagga.dtos.Response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LoginResponse {
+private String message;
+private String token;
+}
---
+++ src/main/java/africa/semicolon/com/quagga/security/config/utils/SecurityUtils.java
@@ -0,0 +1,11 @@
+package africa.semicolon.com.quagga.security.config.utils;
+
+import java.util.List;
+
+public class SecurityUtils {
+    private SecurityUtils () {}
+    public static final List<String> PUBLIC_ENDPOINTS = List.of("/api/v1/auth");
+
+    public static final String JWT_PREFIX = "BEARER ";
+    
+}
