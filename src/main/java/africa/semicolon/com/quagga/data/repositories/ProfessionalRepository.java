90ac15065cbe554fac88044cf9d7e91c9abae486
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-07-31 07:24:39 +0100

    few changes

---
+++ src/main/java/africa/semicolon/com/quagga/data/repositories/ProfessionalRepository.java
@@ -0,0 +1,8 @@
+package africa.semicolon.com.quagga.data.repositories;
+
+import africa.semicolon.com.quagga.data.models.Professional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfessionalRepository extends JpaRepository<Professional, Long> {
+
+}
