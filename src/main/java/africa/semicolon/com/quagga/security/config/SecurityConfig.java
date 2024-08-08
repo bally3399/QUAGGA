0a810e71ee895e03731d92b6c4fe0ced5786098e
josephEmeka <josephemekaonah@gmail.com>
2024-08-08 07:34:49 +0100

    spring security update

--- src/main/java/africa/semicolon/com/quagga/security/config/SecurityConfig.java
+++ src/main/java/africa/semicolon/com/quagga/security/config/SecurityConfig.java
@@ -4,9 +4,10 @@ import africa.semicolon.com.quagga.security.config.filter.CustomAuthorizationFil
 import africa.semicolon.com.quagga.security.config.filter.CustomUsernamePasswordAuthenticationFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
@@ -14,8 +15,10 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
-@Configuration
+@EnableWebSecurity
 @AllArgsConstructor
+@EnableMethodSecurity(prePostEnabled = true)
+
 public class SecurityConfig {
     private final AuthenticationManager authenticationManager;
     private final CustomAuthorizationFilter authorizationFilter;
@@ -24,15 +27,18 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         var   authFilter = new CustomUsernamePasswordAuthenticationFilter(authenticationManager) ;
-        authFilter.setFilterProcessesUrl("/api/v1/auth");
+        authFilter.setFilterProcessesUrl("/api/v1/auth/login");
         return httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
                 .sessionManagement(c-> c.sessionCreationPolicy(STATELESS))
                 .addFilterAt(authFilter, BasicAuthenticationFilter.class)
                 .addFilterBefore(authorizationFilter, CustomUsernamePasswordAuthenticationFilter.class)
-                .authorizeHttpRequests(c->c.requestMatchers(POST, "/api/v1/auth").permitAll()
-                        .requestMatchers( "/api/v1/media").hasAuthority("USER"))
-
+                .authorizeHttpRequests(c -> c
+                        .requestMatchers(POST, "/api/v1/auth/login").permitAll()
+                        .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/v1/user/**").hasAnyRole("USER", "ADMIN")
+                        .anyRequest().authenticated()
+                )
                 .build();
     }
 }
